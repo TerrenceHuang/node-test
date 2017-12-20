4e54bf8163101c75137034b3883298640bc170d6
terrence-huang <terrencehuang2000@gmail.com>
2017-12-20 11:01:34 +0800

    get with header

---
+++ http/client/request.js
@@ -0,0 +1,25 @@
+const http = require('http');
+
+var options = { 
+  hostname: 'localhost',
+  port: 3000,
+  path: '/header',
+  method: 'GET',
+  headers: {'Cookie': 'myCookie=myvalue'}
+};
+var results = ''; 
+var req = http.request(options, function(res) {
+  res.on('data', function (chunk) {
+      results = results + chunk;
+      console.log(results);
+  }); 
+  res.on('end', function () {
+      //TODO
+  }); 
+});
+
+req.on('error', function(e) {
+      console.log(e);
+});
+
+req.end();
--- http/server/index.js
+++ http/server/index.js
@@ -1,6 +1,16 @@
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res) => res.send('Hello World!'));
+
+
+app.get('/', (req, res) => {
+  console.log(req);
+  res.send('Hello World!')
+});
+
+app.get('/header', (req, res) => {
+  console.log(req.get('Cookie'));
+  res.send('header!');
+});
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'));
